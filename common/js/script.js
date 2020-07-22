8b0ce841f0399901865fa2d0e4cba1d66fa91195
NQ.TRUNG <46063989+trung123@users.noreply.github.com>
2020-07-22 16:04:15 +0700

    s
    s

--- common/js/script.js
+++ common/js/script.js
@@ -1,20 +1,36 @@
 $(document).ready(function() {
+    var $width_body = $("body").width();
+
+    if($height_body < 951 ) {
+        $('.gr-tg-humbarger').hover(function(){
+            $(this).addClass('active');
+            $('.gr-tg-menuclick,.gr-tg-bg-op-active').addClass('active');
+        });
+
+        $('.gr-tg-bg-op-active').hover(function(){
+            $(this).removeClass('active');
+            $('.gr-tg-menuclick,.gr-tg-humbarger').removeClass('active');
+        });
+    }
 
     $("header").height($(".gr-tg-top-head").outerHeight());
 
-    $('.gr-tg-humbarger').hover(function(){
-        $(this).addClass('active');
-        $('.gr-tg-menuclick,.gr-tg-bg-op-active').addClass('active');
-    });
-
-    $('.gr-tg-bg-op-active').hover(function(){
-        $(this).removeClass('active');
-        $('.gr-tg-menuclick,.gr-tg-humbarger').removeClass('active');
-    });
-
+    
     $('.gr-tg-tab > ul > li').click(function(){
         $('.gr-tg-tab > ul > li').removeClass('active');
         $(this).addClass('active');
+    });
+
+    $('.gr-tg-humbarger').click(function(){
+        console.log('123');
+        $(this).toggleClass('active');
+        $('.gr-tg-menuclick,.gr-tg-bg-op-active').toggleClass('active');
+    }); 
+
+
+    $('.gr-tg-bg-op-active').click(function(){
+        $(this).removeClass('active');
+        $('.gr-tg-menuclick,.gr-tg-humbarger').removeClass('active');
     });
 
     var topBtn = $('.gr-tg-scroll'); 
