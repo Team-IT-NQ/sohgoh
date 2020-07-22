95140aaafa62e174af229b6f163bc5727ba0de33
NQ.TRUNG <46063989+trung123@users.noreply.github.com>
2020-07-22 19:27:26 +0700

    s
    s

--- common/js/script.js
+++ common/js/script.js
@@ -1,18 +1,5 @@
 $(document).ready(function() {
-    var $width_body = $("body").width();
-
-    if($height_body < 951 ) {
-        $('.gr-tg-humbarger').hover(function(){
-            $(this).addClass('active');
-            $('.gr-tg-menuclick,.gr-tg-bg-op-active').addClass('active');
-        });
-
-        $('.gr-tg-bg-op-active').hover(function(){
-            $(this).removeClass('active');
-            $('.gr-tg-menuclick,.gr-tg-humbarger').removeClass('active');
-        });
-    }
-
+    
     $("header").height($(".gr-tg-top-head").outerHeight());
 
     
